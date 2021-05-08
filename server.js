56f48b258c482e4aea2a6a727efcc434b33c616e
Elyas <touahria.elyas@outlook.com>
2021-05-08 17:40:15 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+//HTTP
+//Serveur Express pour la partie http (sert juste a servir les pages du site)
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+//middleware
+app.use(express.static('public'));
+
+//routes
+app.get("/",(req,res)=>{
+    res.render('index.html');
+});
+
+//Ecoute du port
+app.listen(PORT,()=>{
+     console.log('le server est lancé');
+ });
